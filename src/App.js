08d6b0efa183299f840629e5fd7aadf22349251c
Mikael Engvall <mikael.engvall.me@gmail.com>
2025-01-09 17:04:37 +0100

    reducer

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Error from "./Error";
+import Loader from "./Loader";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Invalid action type");
+  }
+}
+export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+      </Main>
+    </div>
+  );
+}
